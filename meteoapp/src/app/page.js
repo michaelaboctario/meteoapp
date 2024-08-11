d1930631aba785d37bf5322bdf89b1e7854b484e
Michaela Bucatariu <mbucatariu24@gmail.com>
2024-08-11 16:14:05 +0200

    add styles.css

--- meteoapp/src/app/page.js
+++ meteoapp/src/app/page.js
@@ -1,9 +1,9 @@
 'use client'
 
 import Image from "next/image";
-//import "./styles.css";
 import { useState } from 'react';
 import axios from 'axios';
+import "./styles.css";
 
 const URL = 'https://api.openweathermap.org/data/2.5/weather';
 const API_KEY = '7363b6f39c6a94e29aab8070c131232f';
@@ -32,47 +32,41 @@ export default function MeteApp() {
                       lang: 'fr',
                   },
               })
-              .then((res) => {
+              .then(res => {
                   console.log('res', res);
                   setMeteo({ data: res.data, loading: false, error: false });
               })
-              .catch((error) => {
-                  setMeteo({ ...weather, data: {}, error: true });
+              .catch(error => {
+                  setMeteo({ ...meteo, data: {}, error: true });
                   setInput('');
                   console.log('error', error);
               });
       }
   }
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-          Application données météo&nbsp;
-          {/* <code className="font-mono font-bold">src/app/page.js</code> */}
-          <h1 className="app-name">
+      <div className="app">
+            <h1 className="app-name">
                Météo Application
             </h1>
-            <div className="search-bar">
+            <div className="lookup-bar">
                 <input
                     type="text"
                     className="ville-lookup"
                     placeholder="Le nom de la ville.."
                     name="query"
                     value={input}
-                    onChange={(e) => setInput(e.target.value)}
+                    onChange={e => setInput(e.target.value)}
                     onKeyDown={search}
                 />
             </div>
-      </div>
-      {meteo && meteo.data && meteo.data.main && (
+            <div className="meteo-card">
+              {meteo && meteo.data && meteo.data.main && (
                 <div>
-                    <div className="city-name">
+                    <div className="ville-nom">
                         <h2>
                             {meteo.data.name}, <span>{meteo.data.sys.country}</span>
                         </h2>
                     </div>
-                    {/* <div className="date">
-                        <span>{toDateFunction()}</span>
-                    </div> */}
                     <div className="icon-temp">
                         <img
                             className=""
@@ -83,11 +77,13 @@ export default function MeteApp() {
                         <sup className="deg">°C</sup>
                     </div>
                     <div className="des-wind">
-                        <p>{meteo.data.weather[0].description.toUpperCase()}</p>
+                        <p>{meteo.data.weather[0].description}</p>
                         <p>Vitesse du vent: {meteo.data.wind.speed}m/s</p>
                     </div>
                 </div>
-            )}
-    </main>
+              )}
+            </div>
+            
+      </div>
   );
 }
